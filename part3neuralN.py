4113fc9fd8461e91463dedad369ad0f461b59086
Arnav Sharma <67711082+ARNisUsername@users.noreply.github.com>
2020-11-04 18:21:38 -0800

    Update part3neuralN.py

--- part3neuralN.py
+++ part3neuralN.py
@@ -9,6 +9,7 @@ df = pd.read_csv('updatedBased2.csv')[350:]
 X = df['message']
 y = df['is_based']
 
+#--------- FIRST WAY-----
 #Filtering and tokenizing of stopwords(useless words)
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(X)
@@ -19,6 +20,14 @@ X_train_tf = tf_transformer.transform(X_train_counts)
 
 #Train the model using MultinomialNB and the updated X_train
 clf = MLPClassifier(solver='lbfgs',hidden_layer_sizes=[10,10],random_state=0).fit(X_train_tf, y)
+
+#-------SECOND WAY----
+clf = Pipeline([
+                ('count_vect',CountVectorizer()),
+                ('tfidf',TfidfTransformer(use_idf=False)),
+                ('clf',MLPClassifier(solver='lbfgs',hidden_layer_sizes=[11,11],random_state=0,max_iter=10000))
+])
+clf.fit(X, y)
 
 def changeString(theStr):
     docs = [theStr]
